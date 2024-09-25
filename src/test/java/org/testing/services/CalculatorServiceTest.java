2407b5ca231cd3d0a3202cab9b5b565fa9492036
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2024-09-25 23:11:27 +0530

    @BeforClass class it extecute before any @Test

--- src/test/java/org/testing/services/CalculatorServiceTest.java
+++ src/test/java/org/testing/services/CalculatorServiceTest.java
@@ -1,9 +1,21 @@
 package org.testing.services;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class CalculatorServiceTest {
+
+
+//    before class is used to initialize previous @BeforeClass
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("Before Class");
+        System.out.println("Start test : "+new Date());
+    }
 
 //    test methods.
 
@@ -17,7 +29,7 @@ public class CalculatorServiceTest {
     @Test
     public void calculateAnyNumberTest(){
         int result = CalculatorService.sumAnyNumbers(2,3,4,5,6);
-        int expected = 2;
+        int expected = 20;
         Assert.assertEquals(expected, result);
 
     }
