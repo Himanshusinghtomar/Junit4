a2f524487b4ba44c5b723c05eb1923a49647a3fe
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2024-09-25 23:30:31 +0530

    you can set timeout for the test

--- src/test/java/org/testing/services/CalculatorServiceTest.java
+++ src/test/java/org/testing/services/CalculatorServiceTest.java
@@ -1,11 +1,15 @@
 package org.testing.services;
 
 import org.junit.*;
+import org.junit.rules.Timeout;
 
 import java.util.Date;
 
 public class CalculatorServiceTest {
 
+
+//    counter
+    int counter = 0;
 
 //    before class is used to initialize previous @BeforeClass
 
@@ -17,10 +21,12 @@ public class CalculatorServiceTest {
 
 
     //    after class is used to destroy any file opening or connection
-    @After
-    public void tearDown() throws Exception {
-        System.out.println("After every test cases");
+    @Before
+    public void BeforeEachTestCase() throws Exception {
+        System.out.println("Before every test cases");
+        counter = 0;
     }
+
 
 
 //    test methods.
@@ -28,17 +34,31 @@ public class CalculatorServiceTest {
     @Test
     public void addTwoNumbersTest(){
 
+        for(int i =0;i<10;i++)
+        {
+            counter+= i;
+        }
+
         System.out.println("Add Two Numbers Test");
 
        int result =  CalculatorService.addTwoNumbers(0,5);
 
         Assert.assertEquals(5, result);
+
+        System.out.println("Counter value in two Number: "+counter);
     }
 
 
 
-    @Test
-    public void calculateAnyNumberTest(){
+    @Test(timeout = 2000)
+    public void calculateAnyNumberTest() throws InterruptedException {
+
+        for(int i =0;i<30;i++)
+        {
+            counter+= i;
+        }
+
+        Thread.sleep(3000);
 
         System.out.println("Calculate Any Number");
 
@@ -46,12 +66,14 @@ public class CalculatorServiceTest {
         int expected = 20;
         Assert.assertEquals(expected, result);
 
+        System.out.println("Counter value in Any Number: "+counter);
+
     }
 
     //    after class is used to destroy any file opening or connection
-    @Before
-    public void BeforeEachTestCase() throws Exception {
-        System.out.println("Before every test cases");
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("After every test cases");
     }
 
     @AfterClass
