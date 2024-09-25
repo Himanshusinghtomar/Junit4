341031c25f9db902bf22cb28a9031e0d2876c518
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2024-09-25 23:18:04 +0530

    before class and before and after class and after

--- src/test/java/org/testing/services/CalculatorServiceTest.java
+++ src/test/java/org/testing/services/CalculatorServiceTest.java
@@ -1,8 +1,6 @@
 package org.testing.services;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.Date;
 
@@ -17,20 +15,49 @@ public class CalculatorServiceTest {
         System.out.println("Start test : "+new Date());
     }
 
+
+    //    after class is used to destroy any file opening or connection
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("After every test cases");
+    }
+
+
 //    test methods.
 
     @Test
     public void addTwoNumbersTest(){
+
+        System.out.println("Add Two Numbers Test");
+
        int result =  CalculatorService.addTwoNumbers(0,5);
 
         Assert.assertEquals(5, result);
     }
 
+
+
     @Test
     public void calculateAnyNumberTest(){
+
+        System.out.println("Calculate Any Number");
+
         int result = CalculatorService.sumAnyNumbers(2,3,4,5,6);
         int expected = 20;
         Assert.assertEquals(expected, result);
 
     }
+
+    //    after class is used to destroy any file opening or connection
+    @Before
+    public void BeforeEachTestCase() throws Exception {
+        System.out.println("Before every test cases");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("After all test cases");
+    }
+
+
 }
